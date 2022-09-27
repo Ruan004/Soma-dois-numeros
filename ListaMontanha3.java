514e6f5f966a6d32e98a08ebfe14c9681452a244
Ruan004 <104209771+Ruan004@users.noreply.github.com>
2022-09-27 20:14:31 -0300

    Add files via upload

---
+++ ListaMontanha3.java
@@ -0,0 +1,21 @@
+
+package listamontanha3;
+
+import java.util.Scanner;
+
+
+public class ListaMontanha3 {
+
+
+    public static void main(String[] args) {
+        Scanner leia = new Scanner (System.in);
+        int x, y, soma;
+        System.out.print("Digite o primeiro numero");
+        x = leia.nextInt();
+        System.out.println("Digite o segundo numero");
+        y = leia.nextInt();
+        soma = (x + y);
+        System.out.println("A soma dos numeros e: " + soma);
+    }
+    
+}
